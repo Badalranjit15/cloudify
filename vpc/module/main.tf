533e28520f032e084927bdaec86761c2f0101ed9
Badalranjit15 <65303330+Badalranjit15@users.noreply.github.com>
2022-04-15 10:06:21 +0530

    Update main.tf

--- vpc/module/main.tf
+++ vpc/module/main.tf
@@ -4,10 +4,11 @@ module "vpc" {
   name = var.vpc_name
   cidr = var.vpc_cidr
 
- azs             = data.aws_availability_zones.available.names
+ #azs             = data.aws_availability_zones.available.names
+  azs             = local.availability_zones
  # azs = var.azs
-  private_subnets = var.private_subnets
-  public_subnets  = var.public_subnets
+  private_subnets = local.private_subnets
+ # public_subnets  = var.public_subnets
 
   enable_nat_gateway = var.enable_nat_gateway
   enable_vpn_gateway = var.enable_vpn_gateway
