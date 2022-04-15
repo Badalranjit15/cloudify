1f6c565bec6e85b140d2e75eca10723561f737b7
Badalranjit15 <65303330+Badalranjit15@users.noreply.github.com>
2022-04-15 09:43:01 +0530

    Update variables.tf

--- vpc/module/variables.tf
+++ vpc/module/variables.tf
@@ -16,11 +16,11 @@ variable "vpc_cidr" {
   default     = "10.0.0.0/16"
 }
 
-variable "private_subnets" {
-  type        = list(string)
-  description = "List of private subnets to create within the VPC"
-  default     = ["10.0.1.0/24", "10.0.2.0/24", "10.0.3.0/24"]
-}
+#variable "private_subnets" {
+# type        = list(string)
+#  description = "List of private subnets to create within the VPC"
+#  default     = ["10.0.1.0/24", "10.0.2.0/24", "10.0.3.0/24"]
+#}
 
 variable "enable_nat_gateway" {
   type        = bool
@@ -34,8 +34,42 @@ variable "enable_vpn_gateway" {
   default     = false
 }
 
-variable "azs" {
-  type        = list(string)
-  description = "List of azs"
-  default     = ["${var.region}a", "${var.region}b"]
+#variable "azs" {
+#  type        = list(string)
+#  description = "List of azs"
+ # default     = ["${var.region}a", "${var.region}b"]
+#}
+
+variable "cidr_ab" {
+    type = map
+    default = {
+        development     = "172.22"
+    }
+}
+
+locals {
+    cidr_c_private_subnets  = 1
+
+    max_private_subnets     = 2
+}
+
+data "aws_availability_zones" "available" {
+    state = "available"
+}
+
+locals {
+    availability_zones = data.aws_availability_zones.available.names
+}
+
+variable "environment" {
+    type = string
+    description = "Options: development"
+}
+
+locals {
+    private_subnets = [
+        for az in local.availability_zones : 
+            "${lookup(var.cidr_ab, var.environment)}.${local.cidr_c_private_subnets + index(local.availability_zones, az)}.0/24"
+            if index(local.availability_zones, az) < local.max_private_subnets
+        ]
 }
